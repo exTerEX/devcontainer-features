b6870246432fe7cddc2bb9fce994f221024b2d1d
exTerEX <30706568+exTerEX@users.noreply.github.com>
2024-08-04 03:36:29 +0200

    Updated syntax in tests

--- test/cmake/test.sh
+++ test/cmake/test.sh
@@ -1,52 +1,23 @@
-#!/bin/bash
+!/bin/bash
 
-# This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'CMake' Feature with no options.
-#
-# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
-#
-# Eg:
-# {
-#    "image": "<..some-base-image...>",
-#    "features": {
-#      "color": {}
-#    },
-#    "remoteUser": "root"
-# }
-#
-# Thus, the value of all options will fall back to the default value in the
-# Feature's 'devcontainer-feature.json'.
-# For the 'color' feature, that means the default favorite color is 'red'.
-#
-# These scripts are run as 'root' by default. Although that can be changed
-# with the '--remote-user' flag.
-# 
-# This test can be run with the following command:
-#
-#    devcontainer features test    \ 
-#               --features color   \
-#               --remote-user root \
-#               --skip-scenarios   \
-#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
-#               /path/to/this/repo
+# Run tests for cmake using this command
+# #> devcontainer features test --features cmake --base-image mcr.microsoft.com/devcontainers/base:debian
 
 set -e
 
-# Import test library bundled with the devcontainer CLI
-# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
-# Definition specific tests
+# Feature specific tests
 check "version" cmake  --version
 
-# Check that tools can execute
+# Definition specific tests
 check "cmake" cmake  --version
 check "ccmake" ccmake  --version
 check "cpack" cpack  --version
 check "ctest" ctest  --version
 
-# Check paths in settings
+# File position validation
 check "which cmake" bash -c "which cmake | grep /usr/local/bin/cmake"
 check "which ccmake" bash -c "which ccmake | grep /usr/local/bin/ccmake"
 check "which cpack" bash -c "which cpack | grep /usr/local/bin/cpack"
--- test/ninja-build/install_ninja_1.10.1.sh
+++ test/ninja-build/install_ninja_1.10.1.sh
@@ -1,39 +1,10 @@
-#!/bin/bash
+!/bin/bash
 
-# This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'CMake' Feature with no options.
-#
-# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
-#
-# Eg:
-# {
-#    "image": "<..some-base-image...>",
-#    "features": {
-#      "color": {}
-#    },
-#    "remoteUser": "root"
-# }
-#
-# Thus, the value of all options will fall back to the default value in the
-# Feature's 'devcontainer-feature.json'.
-# For the 'color' feature, that means the default favorite color is 'red'.
-#
-# These scripts are run as 'root' by default. Although that can be changed
-# with the '--remote-user' flag.
-# 
-# This test can be run with the following command:
-#
-#    devcontainer features test    \ 
-#               --features color   \
-#               --remote-user root \
-#               --skip-scenarios   \
-#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
-#               /path/to/this/repo
+# Run tests for ninja-build using this command
+# #> devcontainer features test --features ninja-build --base-image mcr.microsoft.com/devcontainers/base:debian
 
 set -e
 
-# Import test library bundled with the devcontainer CLI
-# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
